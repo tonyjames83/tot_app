385529ff55c5e35f559e11019f9449c8314c4244
John James <jjames83@gmail.com>
2022-07-21 16:29:50 -0500

    init commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
+  # get 'pages/home'
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#home"
 end
